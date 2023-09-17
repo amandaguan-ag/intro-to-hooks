bc787a4076b70656937dac4cf8f9559c3a5b41a1
Amanda Guan <ag.amandaguan@gmail.com>
2023-09-17 09:54:24 -0700

    add useState hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 
 function Counter() {
-  return <React.Fragment></React.Fragment>;
+  const [counter, setCounter] = useState(0);
+  const [hidden, setHidden] = useState(false);
+
+  return (
+    <React.Fragment>
+      {hidden ? <h1>Count Hidden</h1> : <h1>{counter}</h1>}
+      <button onClick={() => setCounter(counter + 1)}>Count!</button>
+      <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
+    </React.Fragment>
+  );
 }
 
 export default Counter;
